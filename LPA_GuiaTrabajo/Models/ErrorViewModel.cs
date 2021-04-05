f6d73680c907853f75463c31e8efc674e43326e1
poxx1 <julianlastrama@outlook.com>
2021-04-05 19:38:22 -0300

    Hello world

---
+++ LPA_GuiaTrabajo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LPA_GuiaTrabajo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
